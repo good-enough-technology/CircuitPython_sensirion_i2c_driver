8454b2418963bad1ba0f36b6185782c767059c58
tyeth <tyethgundry@googlemail.com>
2023-05-24 18:48:20 +0100

    add Circuitpython/Linux(auto) as I2cTransceiver

--- sensirion_i2c_driver/__init__.py
+++ sensirion_i2c_driver/__init__.py
@@ -5,7 +5,13 @@
 from .version import version as __version__  # noqa: F401
 from .device import I2cDevice  # noqa: F401
 from .connection import I2cConnection  # noqa: F401
-from .linux_i2c_transceiver import LinuxI2cTransceiver  # noqa: F401
+
+import sys
+if sys.implementation.name.lower() == "circuitpython":
+    from .circuitpython_i2c_transceiver import CircuitPythonI2cTransceiver as I2cTransceiver
+else:
+    from .linux_i2c_transceiver import LinuxI2cTransceiver as I2cTransceiver
+
 from .command import I2cCommand  # noqa: F401
 from .sensirion_command import SensirionI2cCommand  # noqa: F401
 from .crc_calculator import CrcCalculator  # noqa: F401
